7079e127f13c058fb8a893c21fe468b4b39cbfa0
Sven R. Kunze <kunsv@hrz.tu-chemnitz.de>
2012-10-26 19:06:36 +0200

    bug fixes

--- ckanext/lodstatsext/theme/public/scripts/subscription/semantic.js
+++ ckanext/lodstatsext/theme/public/scripts/subscription/semantic.js
@@ -1,92 +1,4 @@
-document.querySelector('#create_subscription').addEventListener('click', function(){
-    json = {};
-
-    if(document.querySelector('#topics .checkbox').checked)
-    {
-        json['topics'] = [];
-        topic_names = document.querySelectorAll('#topics .topic .name');
-        for (index = 0; index < topic_names.length; index += 1) 
-        {
-            json['topics'].push(topic_names[index].textContent);
-        }
-        
-        if(json['topics'].length == 0)
-        {
-            delete json['topics']
-        }
-    }
-
-    if(document.querySelector('#location .checkbox').checked)
-    {
-        json['location'] = {};
-        json['location']['latitude'] = document.querySelector('#location .latitude').value;
-        json['location']['longitude'] = document.querySelector('#location .longitude').value;
-        json['location']['radius'] = document.querySelector('#location .radius').value;
-        json['location']['geoname'] = document.querySelector('#location .geoname').value;
-    }
-
-    if(document.querySelector('#time .checkbox').checked)
-    {
-        json['time'] = {}
-        if(document.querySelector('#time .time_type_span').checked)
-        {
-            json['time']['type'] = 'span'
-            json['time']['min'] = document.querySelector('#time .min').value;
-            json['time']['max'] = document.querySelector('#time .max').value;
-        }
-        if(document.querySelector('#time .time_type_point').checked)
-        {
-            json['time']['type'] = 'point'
-            json['time']['point'] = document.querySelector('#time .point').value;
-            json['time']['variance'] = document.querySelector('#time .variance').value;
-        }
-    }
-
-    document.create_form.subscription_definition.value = JSON.stringify(json)
-    document.create_form.submit()
-})
-
-
-document.querySelector('#topics .add').addEventListener('click', function(){
-    topic_template_node = document.querySelector('#topics .topic_template')
-    topic_node = topic_template_node.cloneNode(true)
-    topic_node.removeAttribute('style')
-    topic_node.setAttribute('class', 'topic')
-    topic_node.querySelector('.name').textContent = document.querySelector('#topics .input_uri').value
-    topic_node.querySelector('.delete').addEventListener('click', function(){
-        topic_node = this.parentNode
-        topic_node.parentNode.removeChild(topic_node)
-    })
-    
-    topic_template_node.parentNode.insertBefore(topic_node, topic_template_node)
-})
-
-
-topicRemoveButtons = document.querySelectorAll('#topics .topic .delete')
-for(index = 0; index < topicRemoveButtons.length; index += 1)
-{
-    topicRemoveButtons[index].addEventListener('click', function(){
-        topic_node = this.parentNode
-        topic_node.parentNode.removeChild(topic_node)
-    })
-}
-
-
-document.querySelector('#topics .checkbox').addEventListener('click', function(){
-    document.querySelector('#topics .hideable').style['display'] = this.checked ? '' : 'none'
-})
-document.querySelector('#location .checkbox').addEventListener('click', function(){
-    document.querySelector('#location .hideable').style['display'] = this.checked ? '' : 'none'
-})
-document.querySelector('#time .checkbox').addEventListener('click', function(){
-    document.querySelector('#time .hideable').style['display'] = this.checked ? '' : 'none'
-})
-
-
-
-
 var cache = {};
-
 $.ajax(
 {
     url: "/vocabulary",
@@ -99,23 +11,22 @@ $.ajax(
             data[index].id = data[index].vocabulary
         }
         
-        $('#topics .input').autocomplete(
+        $('topic_input').autocomplete(
         {
             source: data,
             minLength: 0,
             focus: function( event, ui ) {
-                $( "#project" ).val( ui.item.title );
+                $("#topic_input").val(ui.item.vocabulary);
                 
                 return false;
             },
             select: function( event, ui )
             {
-                $('#topics .input').val(ui.item.title)
-                $('#topics .input_uri').val(ui.item.vocabulary)
+                $('#topic_input').val(ui.item.vocabulary)
                 
                 return false;
             }
-        }).data( "autocomplete" )._renderItem = function( ul, item )
+        }).data("autocomplete")._renderItem = function( ul, item )
         {
             return $( "<li>" )
                 .data( "item.autocomplete", item )
