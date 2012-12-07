f329d107bbefb9c7a9bc4f31f704334646882afe
Sven R. Kunze <kunsv@hrz.tu-chemnitz.de>
2012-12-07 23:17:00 +0100

    removed unused field

--- ckanext/semantic/model/statistics/dataset_statistics_configuration.py
+++ ckanext/semantic/model/statistics/dataset_statistics_configuration.py
@@ -7,14 +7,12 @@ class DatasetStatisticsConfiguration(domain_object.DomainObject):
     def __init__(self, dataset_id):
         self.dataset_id = dataset_id
         self.created = None
-        self.request_count = 0
 
 
 dataset_statistics_configuration_table = sqlalchemy.Table(
     'dataset_statistics_configuration', meta.metadata,
     sqlalchemy.Column('dataset_id', sqlalchemy.types.UnicodeText, primary_key=True),
     sqlalchemy.Column('created', sqlalchemy.types.DateTime),
-    sqlalchemy.Column('request_count', sqlalchemy.types.Integer)
     )
 
 
