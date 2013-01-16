49c5a8fd972861e222fa431a015c9aa32945e6cf
Sven R. Kunze <kunsv@hrz.tu-chemnitz.de>
2013-01-16 19:45:49 +0100

    Removed cloudmade key

--- ckanext/semantic/theme/public/javascript/package/semantic.js
+++ ckanext/semantic/theme/public/javascript/package/semantic.js
@@ -7,7 +7,7 @@ var radius = parseFloat(map_element.attributes['radius'].value);
 var map = L.map('map');
 map.setView([latitude, longitude], 1);
 
-L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 20
 }).addTo(map);
